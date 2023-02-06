d6ebc9c7a8c9953fbb1b032b9513d7815bb2d564
cryptonomicon46 <sandman2145@gmail.com>
2023-02-05 17:42:00 -0800

    Create Campaigns and check all the info
    Tests implemented

--- contracts/BettingCampaign.sol
+++ contracts/BettingCampaign.sol
@@ -1,18 +1,20 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
+pragma abicoder v2;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "hardhat/console.sol";
 interface ICampaign {
 
     /**
      * @dev enum that selects the Motorcyle race premier league class of the campaign
      */
-    enum PremierLeague {Ignore,MotoGp, Moto2, WSBK}
+    enum PremierLeague {ZERO,MotoGp, Moto2, WSBK}
 
     /**
      * @dev enum defines the stage of any given league, Accepting Bets/Closed/RevealWinner
      */
-    enum Stages {AcceptingBets, Closed, RevealWinner}
+    enum Stages {ZERO, AcceptingBets, Closed, RevealWinner}
 
 
     /**
@@ -22,19 +24,27 @@ interface ICampaign {
         uint256 raceNum;
         uint256 raceDate;
         Stages stage;
+        PremierLeague league;
     }
     /**
-     * createCampaign creates a new MotoGp=1/Moto2=2/WSBK=3 campaign 
+     * @notice createCampaign only Owner of this contract can launch a  MotoGp=1/Moto2=2/WSBK=3 Betting campaign
+     * @param league selects the league of motorcycle racing, which could be MotoGp=1/Moto2=2/WSBK=3 
+     * @param raceNum is the race round for the selected Premier League 
+     * @param raceDate is the race Date 
+     * @return bool true if the campaign is created 
      * 
      */
-    function createCampaign(PremierLeague _league, uint256 raceNum,  uint256 raceDate ) external returns (bool);
+    function createCampaign(PremierLeague league, uint256 raceNum,  uint256 raceDate) external returns (bool);
+
+    function getCampaignInfo(uint256 _campaignId) external returns (CampaignDetails memory);
 
 }
 
 contract BettingCampaign is Ownable, ICampaign {
     using SafeMath for uint256;
     mapping(uint256 => CampaignDetails) private campaign_info;
-    uint256 private _totalCampaigns;
+    uint256 private _totalCampaigns=1;
+  
 
 
 /**
@@ -47,18 +57,32 @@ contract BettingCampaign is Ownable, ICampaign {
 
     /**
      * @notice createCampaign only Owner of this contract can launch a  MotoGp=1/Moto2=2/WSBK=3 Betting campaign
-     * @param _league selects the league of motorcycle racing, which could be MotoGp=1/Moto2=2/WSBK=3 
+     * @param league selects the league of motorcycle racing, which could be MotoGp=1/Moto2=2/WSBK=3 
      * @param raceNum is the race round for the selected Premier League 
      * @param raceDate is the race Date 
      * @return bool true if the campaign is created 
      */
-function createCampaign(PremierLeague _league, uint256 raceNum,  uint256 raceDate ) external virtual override onlyOwner returns (bool) {
-    campaign_info[_totalCampaigns]= CampaignDetails(raceNum,raceDate,Stages.AcceptingBets);
+function createCampaign(PremierLeague league, uint256 raceNum,  uint256 raceDate) external virtual override onlyOwner returns (bool) {
+    // require(league >0 && league<=3,"BettingCampaign: Invalid league ID MotoGp=1/Moto2=2/WSBK=3");
+    campaign_info[_totalCampaigns]= CampaignDetails(raceNum,raceDate,Stages.AcceptingBets, league);
+    console.log(uint(Stages.AcceptingBets));
     _totalCampaigns = _totalCampaigns.add(1);
-    emit CampaignCreated(_league, raceNum, raceDate);
+    emit CampaignCreated(league, raceNum, raceDate);
 }
 
 
-// function getCampaignInfo(uint256 _campaignId) {}
+
+    /**
+     * @notice getCampaignInfo Gets the campaign info for a specified campaign id  
+     * @param _campaignId the ID of the campaign 
+     * @return CampaignDetails returns the campaign details struct that has the raceNum, raceDate and campaign Stage (acceptingBets, Stopped, RevealWinner)
+     */
+    function getCampaignInfo(uint256 _campaignId) external view virtual override returns (CampaignDetails memory){
+        require(_campaignId<= _totalCampaigns,"BettingCampaign: Invalid campaign ID number");
+        // console.log("Stage:", campaign_info[_campaignId].stage);
+        return campaign_info[_campaignId];
+
+    }
+
 
 }
--- test/1_testCampaigns.js
+++ test/1_testCampaigns.js
@@ -13,11 +13,16 @@ const {
   isCallTrace,
 } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 
-const motoGp_date = new Date("1/2/2023");
-const moto2_date = new Date("1/10/2023");
-const wsbk_date = new Date("1/18/2023");
+const [motoGp_date, moto2_date, wsbk_date] = [
+  new Date("1/2/2023"),
+  new Date("1/10/2023"),
+  new Date("1/18/2023"),
+];
 
-describe("BettingCampaign", function () {
+const [motoGP_ID, moto2_ID, wsbk_ID] = [1, 2, 3];
+const [AcceptingBets, Closed, RevealWinner] = [1, 2, 3];
+
+describe("BettingCampaign: Basic tests", function () {
   async function deployFixture() {
     [owner, addr1, addr2] = await ethers.getSigners();
     const BettingCampaign = await ethers.getContractFactory("BettingCampaign");
@@ -66,5 +71,104 @@ describe("BettingCampaign", function () {
     )
       .to.emit(bettingCampaign, "CampaignCreated")
       .withArgs(1, 1, Date.parse(motoGp_date));
+  });
+});
+
+describe("BettingCampaign: Launch campaigns", function () {
+  async function deployFixture() {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    const BettingCampaign = await ethers.getContractFactory("BettingCampaign");
+    const bettingCampaign = await BettingCampaign.deploy();
+    await bettingCampaign.deployed();
+
+    return { owner, addr1, addr2, bettingCampaign };
+  }
+
+  it("Launch MotoGp Campaign: Create a campaign and get back campaign Info", async function () {
+    const { owner, addr1, bettingCampaign } = await loadFixture(deployFixture);
+
+    await expect(
+      bettingCampaign
+        .connect(owner)
+        .createCampaign(motoGP_ID, 1, Date.parse(motoGp_date))
+    )
+      .to.emit(bettingCampaign, "CampaignCreated")
+      .withArgs(motoGP_ID, 1, Date.parse(motoGp_date));
+
+    const [raceNum, raceDate, raceStage, raceLeague] =
+      await bettingCampaign.getCampaignInfo(1);
+
+    expect(raceNum).to.be.equal(BigNumber.from("1"));
+    expect(raceDate).to.be.equal(Date.parse(motoGp_date));
+    expect(raceStage).to.be.equal(BigNumber.from(AcceptingBets));
+    expect(raceLeague).to.be.equal(BigNumber.from(motoGP_ID));
+  });
+
+  it("Launch Moto2 Campaign: Create a Moto2 campaign and get back campaign Info", async function () {
+    const { owner, addr1, bettingCampaign } = await loadFixture(deployFixture);
+    //Create MotoGP Campaign =1
+    await expect(
+      bettingCampaign
+        .connect(owner)
+        .createCampaign(motoGP_ID, 1, Date.parse(motoGp_date))
+    )
+      .to.emit(bettingCampaign, "CampaignCreated")
+      .withArgs(motoGP_ID, 1, Date.parse(motoGp_date));
+
+    //Create Moto2 campaign =2
+    await expect(
+      bettingCampaign
+        .connect(owner)
+        .createCampaign(moto2_ID, 1, Date.parse(moto2_date))
+    )
+      .to.emit(bettingCampaign, "CampaignCreated")
+      .withArgs(moto2_ID, 1, Date.parse(moto2_date));
+    const [raceNum, raceDate, raceStage, raceLeague] =
+      await bettingCampaign.getCampaignInfo(2);
+    // console.log(await bettingCampaign.getCampaignInfo(2));
+
+    expect(raceNum).to.be.equal(BigNumber.from("1"));
+    expect(raceDate).to.be.equal(Date.parse(moto2_date));
+    expect(raceStage).to.be.equal(BigNumber.from(AcceptingBets));
+    expect(raceLeague).to.be.equal(BigNumber.from(moto2_ID));
+  });
+
+  it("Launch WSBK Campaign: Create a campaign and get back campaign Info", async function () {
+    const { owner, addr1, bettingCampaign } = await loadFixture(deployFixture);
+
+    //Create MotoGP Campaign =1
+    await expect(
+      bettingCampaign
+        .connect(owner)
+        .createCampaign(motoGP_ID, 1, Date.parse(motoGp_date))
+    )
+      .to.emit(bettingCampaign, "CampaignCreated")
+      .withArgs(motoGP_ID, 1, Date.parse(motoGp_date));
+
+    //Create Moto2 campaign =2
+    await expect(
+      bettingCampaign
+        .connect(owner)
+        .createCampaign(moto2_ID, 1, Date.parse(moto2_date))
+    )
+      .to.emit(bettingCampaign, "CampaignCreated")
+      .withArgs(moto2_ID, 1, Date.parse(moto2_date));
+
+    //Create WSBK campaign
+    await expect(
+      bettingCampaign
+        .connect(owner)
+        .createCampaign(wsbk_ID, 1, Date.parse(wsbk_date))
+    )
+      .to.emit(bettingCampaign, "CampaignCreated")
+      .withArgs(wsbk_ID, 1, Date.parse(wsbk_date));
+
+    const [raceNum, raceDate, raceStage, raceLeague] =
+      await bettingCampaign.getCampaignInfo(3);
+
+    expect(raceNum).to.be.equal(BigNumber.from("1"));
+    expect(raceDate).to.be.equal(Date.parse(wsbk_date));
+    expect(raceStage).to.be.equal(BigNumber.from(AcceptingBets));
+    expect(raceLeague).to.be.equal(BigNumber.from(wsbk_ID));
   });
 });
